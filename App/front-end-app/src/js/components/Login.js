e227e17a24f39a9738b5e1fc6b862408958c36c4
eriakul <lu.erika58@gmail.com>
2019-07-19 18:57:01 -0400

    login page

--- App/front-end-app/src/js/components/Login.js
+++ App/front-end-app/src/js/components/Login.js
@@ -72,10 +72,6 @@ export default function SignIn() {
                         id="password"
                         autoComplete="current-password"
                     />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
                     <Button
                         type="submit"
                         fullWidth
