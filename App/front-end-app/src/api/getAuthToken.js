9d1ea42989939361600cdd4077d4b00254de8e74
eriakul <lu.erika58@gmail.com>
2019-07-25 22:49:02 -0400

    running app gets lines

--- App/front-end-app/src/api/getAuthToken.js
+++ App/front-end-app/src/api/getAuthToken.js
@@ -2,19 +2,13 @@
 import 'fetch'
 
 export default (email, password) => {
-    const url = 'https://tinderappgetlinesforname.azurewebsites.net';
+    const url = 'https://tinderapp.azurewebsites.net/api/getLinesForName';
     const data = { name: "jennifer" };
 
     const response = fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
-        headers: {
-            // 'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': "http://localhost:3000",
-            'Accept': 'application/json',
-            'Origin': 'http://localhost:3000'
-        }
-    }).then(response => { response.text() }).then(contents => console.log(contents))
+    }).then(response => { return response.json() }).then(contents => console.log(contents))
     return response
 
 }
