74cee61a72be05a3fbb8d1604d9e34a366ab7863
Jared Entwistle <jce46@case.edu>
2019-07-31 19:55:23 -0500

    Add code to connect to Database and print values in table_1

---
+++ APPys/addLinetoDatabase/addLinesToDatabase/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import pyodbc
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return add_line()
+        #return func.HttpResponse(f"Hello {name}!")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
+
+def add_line():
+    server = 'tinderappdatabase.database.windows.net'
+    database = 'tinderappdatabase'
+    username = 'timlucian0817'
+    password = 'Totoro123!'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    connection = pyodbc.connect('DRIVER='+driver+'; PORT=1433; SERVER='+server+'; DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = connection.cursor()
+
+    Query = "SELECT *  from tinderappdatabase.dbo.Table_1"
+
+    cursor.execute(Query)
+    row = cursor.fetchone()
+    tableString = "Table: "
+    while row:
+        tableString = tableString + "\n" + (str(row[0]) + " " + str(row[1]) + " " + str(row[2]))
+        row = cursor.fetchone()
+    
+    return tableString
